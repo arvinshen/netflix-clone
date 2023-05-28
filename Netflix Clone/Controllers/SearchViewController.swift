48e0ca3819fa1e07d9fb1764f9b0188ed89eee6b
arvinshen <ashen3292@gmail.com>
2023-05-28 13:17:29 -0700

    Add tab bar view controller

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Arvin Shen on 5/27/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Arvin Shen on 5/27/23.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
